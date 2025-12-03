0601026576b4ff244bc0acddc404b599d5504a1e
RASUL YEA BARI SUNBEAM <sunbeam20@outlook.com>
2025-12-03 18:48:48 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+      eslint: {
+    ignoreDuringBuilds: true,  // ⚠️ This will ignore ALL ESLint errors
+  },
 };
 
 export default nextConfig;
